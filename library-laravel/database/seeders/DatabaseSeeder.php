1526b26c68e20f4d68c05c737c0358fe1a8257a2
enabogucanin <eb20210287@student.fon.bg.ac.rs>
2025-09-28 15:29:26 +0200

    factories

--- library-laravel/database/seeders/DatabaseSeeder.php
+++ library-laravel/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Genre;
+use App\Models\User;
+use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,11 +16,60 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(5)->create();
+        Author::factory(10)->create();
+        Genre::factory(5)->create();
+        Book::factory(2)->create([
+            'author_id' => 1,
+            'genre_id' => 1,
+        ]);
+        Book::factory(3)->create([
+            'author_id' => 2,
+            'genre_id' => 1,
+        ]);
+        Book::factory(2)->create([
+            'author_id' => 3,
+            'genre_id' => 2,
+        ]);
+        Book::factory(3)->create([
+            'author_id' => 4,
+            'genre_id' => 2,
+        ]);
+        Book::factory(2)->create([
+            'author_id' => 5,
+            'genre_id' => 3,
+        ]);
+        Book::factory(3)->create([
+            'author_id' => 6,
+            'genre_id' => 3,
+        ]);
+        Book::factory(2)->create([
+            'author_id' => 7,
+            'genre_id' => 4,
+        ]);
+        Book::factory(3)->create([
+            'author_id' => 8,
+            'genre_id' => 4,
+        ]);
+        Book::factory(2)->create([
+            'author_id' => 9,
+            'genre_id' => 5,
+        ]);
+        Book::factory(3)->create([
+            'author_id' => 10,
+            'genre_id' => 5,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            AuthorSeeder::class,
+        ]);
+
+        $this->call([
+            GenreSeeder::class,
+        ]);
+
+        $this->call([
+            BookSeeder::class,
+        ]);
     }
 }

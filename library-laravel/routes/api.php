a9507a49c62cfb9afc92a4f1b4f32e25f46c341c
enabogucanin <eb20210287@student.fon.bg.ac.rs>
2025-09-29 21:25:47 +0200

    controller_routes_book

--- library-laravel/routes/api.php
+++ library-laravel/routes/api.php
@@ -14,12 +14,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/filter', [BookController::class, 'filter']);
+Route::get('/books/search', [BookController::class, 'search']);
+Route::get('/books/page/{page}', [BookController::class, 'pagination']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    
+     Route::resource('books', BookController::class)
+        ->only(['store', 'update', 'destroy']);
+
 
     Route::post('/logout', [AuthController::class, 'logout']);
 });

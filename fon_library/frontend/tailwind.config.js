147dec8382e60224253383ec6dbe2ed0cb003c50
Andjela151 <ab20210322@student.fon.bg.ac.rs>
2025-10-05 23:21:46 +0200

    styling

---
+++ fon_library/frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        purple: '#8d27ae',
+        pink: '#d9176d',
+        grey: '#f3f3f3',
+        lightBlack: '#717171',
+        blackCustom: '#010101',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      transitionProperty: {
+        all: 'all',
+      },
+    },
+  },
+  plugins: [],
+};
